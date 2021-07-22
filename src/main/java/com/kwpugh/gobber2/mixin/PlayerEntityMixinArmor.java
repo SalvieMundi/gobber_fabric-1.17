5c4e01d9748506d0acf3cc698d95faa14363a9c4
Kevin Pugh <kwpugh@comcast.net>
2021-07-22 15:36:56 -0500

    update

--- src/main/java/com/kwpugh/gobber2/mixin/PlayerEntityMixinArmor.java
+++ src/main/java/com/kwpugh/gobber2/mixin/PlayerEntityMixinArmor.java
@@ -3,10 +3,13 @@ package com.kwpugh.gobber2.mixin;
 import com.kwpugh.gobber2.api.ArmorRemoveHandler;
 import com.kwpugh.gobber2.api.ArmorTickable;
 import com.kwpugh.gobber2.util.ItemUtils;
+import com.kwpugh.gobber2.util.PlayerSpecialAbilities;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.util.collection.DefaultedList;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
@@ -14,6 +17,8 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.UUID;
 
 /**
  * Adapted from TechReborn - credit to the authors
@@ -35,6 +40,21 @@ public abstract class PlayerEntityMixinArmor extends LivingEntity
     @Inject(method = "tick", at = @At("HEAD"))
     public void gobberTickArmor(CallbackInfo ci)
     {
+        // Special perk for Kevin, if wanted
+        PlayerEntity self = (PlayerEntity) (Object) this;
+        ItemStack mainHand = self.getMainHandStack();
+        Item offHand = self.getOffHandStack().getItem();
+        UUID kwpugh = UUID.fromString("10d71fA6-B516-452f-9B1C-F2C447D69E60");
+
+        if(self.getUuid().equals(kwpugh) && offHand == Items.DIAMOND)
+        {
+            PlayerSpecialAbilities.giveHealing(this.world, self, mainHand, 5);
+            PlayerSpecialAbilities.giveSaturationEffect(this.world, self, mainHand);
+            PlayerSpecialAbilities.giveGreaterAbsorption(this.world, self, mainHand);
+            PlayerSpecialAbilities.giveCuringEffect(this.world, self);
+        }
+
+        // Check if armor has been removed, used with flight function
         int i = 0;
         for (ItemStack stack : getArmorItems())
         {
