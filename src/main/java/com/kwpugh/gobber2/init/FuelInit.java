6cc6f557501690e2099f2ed7a7bc6dfc8e86133e
Kevin Pugh <kwpugh@comcast.net>
2021-10-06 11:42:08 -0500

    updates

--- src/main/java/com/kwpugh/gobber2/init/FuelInit.java
+++ src/main/java/com/kwpugh/gobber2/init/FuelInit.java
@@ -1,15 +1,15 @@
 package com.kwpugh.gobber2.init;
 
-import net.fabricmc.fabric.impl.content.registry.FuelRegistryImpl;
+import net.fabricmc.fabric.api.registry.FuelRegistry;
 
 public class FuelInit 
 {
 	public static void registerFuels() 
 	{
-		FuelRegistryImpl registry = FuelRegistryImpl.INSTANCE;
+		FuelRegistry registry = FuelRegistry.INSTANCE;
 
 		registry.add(ItemInit.GOBBER2_FOO, 12000);
 		registry.add(ItemInit.GOBBER2_FOO_NETHER, 24000);
-		registry.add(ItemInit.GOBBER2_FOO_END, 32000);	
+		registry.add(ItemInit.GOBBER2_FOO_END, 32000);
 	}
 }
